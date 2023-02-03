74001a02a3717fd2e226de9afc08ac74249152ec
sorabot <admin@soramitsu.co.jp>
2023-02-03 07:04:23 +0000

    copy

--- src/store/soraCard/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Subscription } from 'rxjs';
-import { FPNumber } from '@sora-substrate/util';
-
-export type SoraCardState = {
-  euroBalance: string;
-  totalXorBalance: FPNumber;
-  xorToDeposit: FPNumber;
-  totalXorBalanceUpdates: Nullable<Subscription>;
-};
